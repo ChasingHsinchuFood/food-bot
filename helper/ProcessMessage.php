b3a4ab8264d854555f550baabd66e453990e242e
peter279k <peter279k@gmail.com>
2016-09-16 16:45:55 +0800

    modify gif url

--- helper/ProcessMessage.php
+++ helper/ProcessMessage.php
@@ -126,7 +126,7 @@
                 $dataLen = count($data) - 1;
                 srand();
                 $index = rand(0, $dataLen);
-                return $data[$index]["url"];
+                return $data[$index]["user"]["avatar_url"];
             }
             else {
                 return "https://valleytechnologies.net/wp-content/uploads/2015/07/error.png";
