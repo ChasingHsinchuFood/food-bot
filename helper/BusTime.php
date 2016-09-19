1c21648b5b260d15625acd507956fdc2b4b0ab12
peter279k <peter279k@gmail.com>
2016-09-19 20:13:14 +0800

    debug output $result

--- helper/BusTime.php
+++ helper/BusTime.php
@@ -76,21 +76,21 @@
                             if(isset($estJson[$estIndex]["StopStatus"])) {
                                 switch($estJson[$estIndex]["StopStatus"]) {
                                     case 1:
-                                        $result["direction_0_stop_name"]["stop_status"] = "尚未發車";     
+                                        $result["direction_0_stop_name"][$resultZeroIndex]["stop_status"] = "尚未發車";     
                                         break;
                                     case 2:
-                                        $result["direction_0_stop_name"]["stop_status"] = "交管不停靠";
+                                        $result["direction_0_stop_name"][$resultZeroIndex]["stop_status"] = "交管不停靠";
                                         break;
                                     case 3:
-                                        $result["direction_0_stop_name"]["stop_status"] = "末班車已過";
+                                        $result["direction_0_stop_name"][$resultZeroIndex]["stop_status"] = "末班車已過";
                                         break;
                                     case 4:
-                                        $result["direction_0_stop_name"]["stop_status"] = "今日未營運";
+                                        $result["direction_0_stop_name"][$resultZeroIndex]["stop_status"] = "今日未營運";
                                         break;
                                 }
                             }
                             else {
-                                $result["direction_0_stop_name"]["stop_status"] = "正常發車";
+                                $result["direction_0_stop_name"][$resultZeroIndex]["stop_status"] = "正常發車";
                             }
 
                             $result["direction_0_stop_name"][$resultZeroIndex] = $stopName;
@@ -98,6 +98,8 @@
                             $result["direction_0_stop_name"]["est_time"][$resultZeroIndex] = $estJson[$estIndex]["EstimateTime"];
 
                             $resultZeroIndex += 1;
+
+                            break;
                         }
                     }
                 }
