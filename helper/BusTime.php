883635dd19a51930003381e2fd60480f1afb6342
peter279k <peter279k@gmail.com>
2016-09-19 12:27:28 +0800

    add est time (initial)

--- helper/BusTime.php
+++ helper/BusTime.php
@@ -48,7 +48,43 @@
                 return "查無此資訊！";
             }
             else {
+                $result = array();
+                $resultIndex = 0;
+
+                $routeZero = $routeJson["direction_0"];
+                $routeOne = $routeJson["direction_1"];
+
+                $result["direction_0_text"] = $routeZero;
+                $result["direction_1_text"] = $routeOne;
                 
+                $dirZeroLen = count($routeJson["Direction_0"]);
+                $dirOneLen = count($routeJson["Direction_1"]);
+
+                $estLen = count($estJson);
+
+                for($routeIndex=0;$routeIndex<$dirZeroLen;$routeIndex++) {
+                    $stopName = $routeJson["Direction_0"][$routeIndex];
+                    for($estIndex=0;$estIndex<$estLen;$estIndex++) {
+                        if($stopName == $estJson[$estIndex]["StopName"] && $estJson[$estIndex]["Direction"] == 0) {
+                            $result["direction_0_stop_name"][$resultIndex] = $estJson[$estIndex]["StopName"];
+                            $result["direction_0_stop_name"]["est_time"][$resultIndex] = $estJson[$estIndex]["EstimateTime"];
+                            $resultIndex += 1;
+                        }
+                    }
+                }
+
+                for($routeIndex=0;$routeIndex<$dirOneLen;$routeIndex++) {
+                    $stopName = $routeJson["Direction_1"][$routeIndex];
+                    for($estIndex=0;$estIndex<$estLen;$estIndex++) {
+                        if($stopName == $estJson[$estIndex]["StopName"] && $estJson[$estIndex]["Direction"] == 1) {
+                            $result["direction_1_stop_name"][$resultIndex] = $estJson[$estIndex]["StopName"];
+                            $result["direction_1_stop_name"]["est_time"][$resultIndex] = $estJson[$estIndex]["EstimateTime"];
+                            $resultIndex += 1;
+                        }
+                    }
+                }
+
+                return $result;
             }
         }
 
@@ -98,10 +134,16 @@
             }
 
             try {
+                $res = array();
                 $response = $client->request("GET", $reqUrl);
                 $result = json_decode($response->getBody(), true);
                 if(strlen($result) === 0) {
                     $result = "no-data";
+                }
+                else {
+                    $res["direction_0"] = $result[strlen($result)-1]["DepartureStopNameZh"];
+                    $res["direction_1"] = $result[strlen($result)-1]["DestinationStopNameZh"];
+                    $result = $res;
                 }
             }
             catch(GuzzleHttp\Exception\ServerException $e) {
