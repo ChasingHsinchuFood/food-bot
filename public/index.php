02f566438a7a3a092ae4a8857567cd539c5d9a90
peter279k <peter279k@gmail.com>
2017-11-25 19:55:36 +0800

    set confidence

--- public/index.php
+++ public/index.php
@@ -111,7 +111,7 @@
         $process = new ProcessMessage($message, $sender);
 
         if(isset($data['nlp']['entities']['greetings'])) {
-            if($data['nlp']['entities']['greetings']['confidence'] >= 0.9) {
+            if($data['nlp']['entities']['greetings'][0]['confidence'] >= 0.9) {
                 $json["message"]["text"] = 'Hello!';
             } else {
                 $json["message"]["text"] = 'default logic.';
